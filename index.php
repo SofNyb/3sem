94ffb601ac15f3cbef1be4df1bb0a6bae1c5bd20
snjwi <sofi13a5@edu.zealand.dk>
2022-12-10 20:13:09 +0100

    Head tilføjet på index

--- index.php
+++ index.php
@@ -6,3 +6,24 @@ $blogs = $db->sql("SELECT * FROM blogs");
 foreach ($blogs as $blog){
     echo $blog->blogName . "<br>";
 }
+?>
+
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+
+    <title>Kragebjerggård</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <link href="images/favicon-16x16.png" rel="icon" type="image/x-icon">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <script src="https://cdn.tiny.cloud/1/xssvlu2s8qf9xc5tsggguxuegtb0t783k713q8tpsbc1x6sh/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+</head>
