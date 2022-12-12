a29b75db2d90e578139c52e04997499172b9002e
snjwi <sofi13a5@edu.zealand.dk>
2022-12-12 15:49:39 +0100

    tilføjet fontawesome-link

--- index.php
+++ index.php
@@ -22,6 +22,7 @@ $produkter = $db->sql("SELECT * FROM produkter");
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <script src="https://cdn.tiny.cloud/1/xssvlu2s8qf9xc5tsggguxuegtb0t783k713q8tpsbc1x6sh/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+    <script src="https://kit.fontawesome.com/bc87d7ac19.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
