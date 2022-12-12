40a0f0d7e5e88c34c02eb280cc60cf113ac1e1a6
snjwi <sofi13a5@edu.zealand.dk>
2022-12-12 23:49:58 +0100

    kontakt-info tilføjet

--- includes/footer.php
+++ includes/footer.php
@@ -3,15 +3,15 @@
             <div class="row mt-3">
                 <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
                     <h6 class="fw-bold mb-4">ÅBNINGSTIDER</h6>
-                    <p>Mandag - torsdag: XX.XX-XX.XX</p>
-                    <p>Fredag - søndag: XX.XX-XX.XX</p>
+                    <p>Mandag - torsdag: 08.00-17.00</p>
+                    <p>Fredag - søndag: 08.00-15.00</p>
                 </div>
                 <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
                     <h6 class="fw-bold mb-4">KONTAKT OS</h6>
-                    <p><i class="fas fa-home me-3"></i>VEJNAVN HUSNUMMER</p>
-                    <p><i class="fas fa-home me-3 text-footerfarve"></i>POSTNUMMER BY</p>
-                    <p><i class="fas fa-envelope me-3"></i><a href="mailto:chocolateclub@cc.com" style="text-decoration-color: white; color: white;">E-MAIL-ADRESSE</a></p>
-                    <p><i class="fas fa-phone me-3"></i>TLF-NUMMER</p>
+                    <p><i class="fas fa-home me-3"></i>Kragebjergvej 1</p>
+                    <p><i class="fas fa-home me-3 text-footer"></i>4296 Nyrup</p>
+                    <p><i class="fas fa-envelope me-3"></i><a href="mailto:kragebjerg@kb.com" style="text-decoration-color: white; color: white;">kragebjerg@kb.com</a></p>
+                    <p><i class="fas fa-phone me-3"></i>57 80 32 52</p>
                 </div>
             </div>
         </div>
