a7952a5b9116b445fd44b1b12d2e53d70a400b70
snjwi <sofi13a5@edu.zealand.dk>
2022-12-12 15:45:51 +0100

    header.php opdateret

---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-expand-lg bg-customPrimary">
+    <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand active" href="index.php"><img src="../images/Logo%20v1.png" alt="Kragebjerggård logo"/></a>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="omos.php">Om os</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="kurv.php">Kurv</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
