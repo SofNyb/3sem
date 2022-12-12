503c641894e140f4e517082ed7804dd118607b16
snjwi <sofi13a5@edu.zealand.dk>
2022-12-12 16:46:43 +0100

    footer.php opdateret Footer-farve i bootstrap.scss tilføjet

---
+++ includes/footer.php
@@ -0,0 +1,35 @@
+<footer class="text-center text-lg-start bg-footer text-light pt-1 mt-2">
+        <div class="container text-md-start">
+            <div class="row mt-3">
+                <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                    <h6 class="fw-bold mb-4">ÅBNINGSTIDER</h6>
+                    <p>Mandag - torsdag: XX.XX-XX.XX</p>
+                    <p>Fredag - søndag: XX.XX-XX.XX</p>
+                </div>
+                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                    <h6 class="fw-bold mb-4">KONTAKT OS</h6>
+                    <p><i class="fas fa-home me-3"></i>VEJNAVN HUSNUMMER</p>
+                    <p><i class="fas fa-home me-3 text-footerfarve"></i>POSTNUMMER BY</p>
+                    <p><i class="fas fa-envelope me-3"></i><a href="mailto:chocolateclub@cc.com" style="text-decoration-color: white; color: white;">E-MAIL-ADRESSE</a></p>
+                    <p><i class="fas fa-phone me-3"></i>TLF-NUMMER</p>
+                </div>
+            </div>
+        </div>
+        <div class="container mb-4">
+            <div class="row mt-3 justify-content-center">
+                <div class="col-auto">
+                    <a href="#" target="_blank">
+                        <i class="fa-brands fa-facebook-f fa-3x"></i>
+                    </a>
+                </div>
+                <div class="col-auto">
+                    <a href="#" target="_blank">
+                        <i class="fa-brands fa-instagram fa-3x"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    <div class="text-light text-center p-2">© 2022 Copyright:
+        <a class="text-light text-decoration-none" href="https://mdbootstrap.com/">Kragebjerggård</a>
+    </div>
+</footer>
