65465ed4f14455c8b9ff5c8c82cc6dc439e24f8e
snjwi <sofi13a5@edu.zealand.dk>
2022-12-12 16:18:12 +0100

    Opdateret header.php

--- includes/header.php
+++ includes/header.php
@@ -1,18 +1,19 @@
 <nav class="navbar navbar-expand-lg bg-customPrimary">
-    <div class="container-fluid">
+    <div class="container-fluid px-5 py-2 text-light">
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <a class="navbar-brand active" href="index.php"><img src="../images/Logo%20v1.png" alt="Kragebjerggård logo"/></a>
         <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="omos.php">Om os</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="kurv.php">Kurv</a>
-                </li>
-            </ul>
+            <div class="navbar-nav">
+                <a class="nav-link hidden" aria-current="page" href="#">Mejeriprodukter</a>
+                <a class="nav-link hidden" aria-current="page" href="#">Grøntsager</a>
+                <a class="nav-link" aria-current="page" href="omos.php">Om os</a>
+            </div>
         </div>
+        <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Søg" aria-label="Search">
+        </form>
+        <a class="nav-link" aria-current="page" href="#"><i class="fa-sharp fa-solid fa-basket-shopping fa-2x"></i></a>
     </div>
 </nav>
