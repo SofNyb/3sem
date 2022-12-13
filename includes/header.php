c8bdfb4a69eb14e5c2db4140a3ca8192f392ac7f
snjwi <sofi13a5@edu.zealand.dk>
2022-12-13 08:41:15 +0100

    header menupunkter gjort hvide og større

--- includes/header.php
+++ includes/header.php
@@ -1,14 +1,14 @@
 <nav class="navbar navbar-expand-lg bg-customPrimary">
-    <div class="container-fluid px-5 py-2 text-light">
+    <div class="container-fluid px-5 py-2">
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <a class="navbar-brand active" href="index.php"><img src="../images/Logo%20v1.png" alt="Kragebjerggård logo"/></a>
         <div class="collapse navbar-collapse" id="navbarNav">
             <div class="navbar-nav">
-                <a class="nav-link hidden" aria-current="page" href="#">Mejeriprodukter</a>
-                <a class="nav-link hidden" aria-current="page" href="#">Grøntsager</a>
-                <a class="nav-link" aria-current="page" href="omos.php">Om os</a>
+                <a class="nav-link hidden text-light" aria-current="page" href="#"><h5>Mejeriprodukter</h5></a>
+                <a class="nav-link hidden text-light" aria-current="page" href="#"><h5>Grøntsager</h5></a>
+                <a class="nav-link text-light" aria-current="page" href="omos.php"><h5>Om os</h5></a>
             </div>
         </div>
         <form class="d-flex" role="search">
