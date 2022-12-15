ac4d160f188e2ce090a4b1838d63616395c61a9e
Mark <markphillip1800@gmail.com>
2022-12-15 15:54:59 +0100

    oprettet 404.php

---
+++ 404.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+
+    <title>Kragebjerggård</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <link href="images/favicon-16x16.png" rel="icon" type="image/x-icon">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.typekit.net/rar7flk.css">
+
+    <script src="https://cdn.tiny.cloud/1/xssvlu2s8qf9xc5tsggguxuegtb0t783k713q8tpsbc1x6sh/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+    <script src="https://kit.fontawesome.com/bc87d7ac19.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+<section class="container">
+
+    <div class="my-5">
+        <img src="images/Kragebjerggård404.png">
+    </div>
+
+    <div class="row justify-content-end">
+        <div class="col-auto">
+            <a class="fw-bold text-decoration-underline text-dark" href="index.php"><span class="p-1 bg-dark rounded-2 text-white fw-light me-2"><<</span>Klik her for at vende tilbage</a>
+        </div>
+    </div>
+
+</section>
+
+</body>
+</html>
